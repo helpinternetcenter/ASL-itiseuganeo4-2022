264ea0eaacda96768977b9f1789dba05b4304071
MattiaCas8 <100998981+MattiaCas8@users.noreply.github.com>
2022-06-22 17:09:42 +0200

    navigator

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Navigator from './src/screens/navigator';
 
+
 export default function App() {
   return (
      <Navigator />
