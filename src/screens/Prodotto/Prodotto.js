a16eff8b1e27045ed0785035b02af76e0b5093f8
Triveeee <riccardo,trivellato2004gmail.com>
2022-06-24 00:21:20 +0200

    aggiunta modifica su prodotto

--- src/screens/Prodotto/Prodotto.js
+++ src/screens/Prodotto/Prodotto.js
@@ -1,20 +1,25 @@
 import * as React from 'react';
 import { IconRegistry, Layout} from '@ui-kitten/components';
 import TitleProdotto from '../../components/TitleProdotto/TitleProdotto';
-import ScroolImage from '../../components/ScroolView/ScroolView';
+import ScroolImage from '../../components/ScroolImage/ScroolImage';
 import Description from '../../components/Description/Description';
 import { EvaIconsPack } from '@ui-kitten/eva-icons';
+import { ScrollView } from 'react-native';
+import PriceDescription from '../../components/PriceDescription/PriceDescription';
 
 
 function Prodotto() {
     return (
       <>
         <IconRegistry icons = {EvaIconsPack}></IconRegistry>
-        <Layout>
-          <TitleProdotto name = 'MyProdotto'></TitleProdotto>
-          <ScroolImage></ScroolImage>
-          <Description></Description>
-      </Layout>
+        <ScrollView>
+          <Layout level = '3'>
+            <TitleProdotto name = 'MyProdotto'></TitleProdotto>
+            <ScroolImage></ScroolImage>
+            <Description></Description>
+            <PriceDescription prezzo = '81,90$'></PriceDescription>
+          </Layout>
+        </ScrollView>
       </>
     );
   }
