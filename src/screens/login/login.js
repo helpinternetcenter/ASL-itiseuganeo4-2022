09a6a806729ad160f55499dac65b019a5d9c62cb
MattiaCas8 <100998981+MattiaCas8@users.noreply.github.com>
2022-06-23 10:44:56 +0200

    login

--- src/screens/login/login.js
+++ src/screens/login/login.js
@@ -1,13 +1,43 @@
 import * as React from 'react';
+import { StyleSheet, View } from 'react-native';
 import { ApplicationProvider, Layout, Text } from '@ui-kitten/components';
 
 
 function Login() {
+
+  
     return (
-<Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-    <Text category='h1'>Login </Text>
-  </Layout>
+
+      <View style={styles.row}>
+        <Text style={styles.text} category='h1'>LOGIN</Text>
+
+        
+      </View>
+
+    
+
+      
+
     );
   }
+
+  const styles = StyleSheet.create({
+    row: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'center',
+      flex: 1
+    },
+    text: {
+      margin: 2,
+    },
+    alternativeContainer: {
+      
+      borderRadius: 4,
+      marginVertical: 2,
+      backgroundColor: '#3366FF',
+      alignItems:'center'
+    },
+  });
 
 export default Login;
