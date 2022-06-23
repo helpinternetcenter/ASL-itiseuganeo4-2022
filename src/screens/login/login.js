f8faecfcad4819d8ceddaa619421694aa2d9e8f9
MattiaCas8 <100998981+MattiaCas8@users.noreply.github.com>
2022-06-23 09:07:32 +0200

    login

--- src/screens/login/login.js
+++ src/screens/login/login.js
@@ -5,7 +5,7 @@ import { ApplicationProvider, Layout, Text } from '@ui-kitten/components';
 function Login() {
     return (
 <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-    <Text category='h1'>Login Screen</Text>
+    <Text category='h1'>Pagina Login </Text>
   </Layout>
     );
   }
