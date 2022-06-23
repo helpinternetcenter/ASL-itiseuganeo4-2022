7feaa2424f8b79547380a64aefeddd3407fcd934
MattiaCas8 <100998981+MattiaCas8@users.noreply.github.com>
2022-06-23 12:28:33 +0200

    loginFromHome

--- src/screens/login/login.js
+++ src/screens/login/login.js
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { StyleSheet, View } from 'react-native';
 import { ApplicationProvider, Layout, Text, Input, Button } from '@ui-kitten/components';
 import { TextInput } from 'react-native-web';
 import { NavigationContainer } from '@react-navigation/native';
@@ -7,26 +8,42 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 
 function Login({navigation}) {
-  const [value, setValue] = React.useState('');
+  const [username, setUsername] = React.useState('');
+  const [password,setPassword] = React.useState('');
 
 
   return (
     <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text category='h1'>Log In </Text>
-      <View style={styles.row}>
-          <Text style={styles.text} category='c1'>Usrname</Text>
-      </View>
+      
+        <View style={styles.row}>
+            <Text style={styles.text} category='p2'>Username</Text>
+          </View>
        
       <Input
-        placeholder='Nome'
-        value={value}
-        onChangeText={nextValue => setValue(nextValue)}
-        style={{marginLeft:'20%' , marginRight:'20%', marginBottom:'10%',marginTop:'10%'}}
+        placeholder='Username'
+        value={username}
+        onChangeText={nextValue => setUsername(nextValue)}
+        style={{marginLeft:'20%' , marginRight:'20%', marginBottom:'10%',marginTop:'5%'}}
       />
 
+         <Text category='p2'>Password </Text>
+
+          <Input
+            placeholder='Password'
+            value={password}
+            onChangeText={nextValue => setPassword(nextValue)}
+            style={{marginLeft:'20%', marginRight:'20%', marginBottom:'10%',marginTop:'5%'}}
+          />
+
+      
+      
       <Button onPress={() => navigation.push('HOME')}>
-        Avanti
+        Log In
       </Button>
+
+
+      
     </Layout>
   );
 }
@@ -35,10 +52,14 @@ const styles = StyleSheet.create({
   row: {
     flexDirection: 'row',
     alignItems: 'center',
+    marginTop:'10%',
   },
   text: {
     margin: 2,
+    marginTop:'10%'
   },
+
+
 });
 
 export default Login;
