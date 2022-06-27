edd012b2e64ddd0412e75733c900bf7b1986583a
Davide5710 <davide.the.strong@gmail.com>
2022-06-27 12:55:46 +0200

    aggiunte immagini dinamiche

--- src/components/ScroolImage/ScroolImage.js
+++ src/components/ScroolImage/ScroolImage.js
@@ -1,64 +1,58 @@
 import React from "react";
-import { Layout , ViewPager , RadioGroup , Radio , Card} from "@ui-kitten/components";
+import { Layout, ViewPager, RadioGroup, Radio, Card } from "@ui-kitten/components";
 import { Image } from "react-native";
 import styles from "./ScroolImage.style";
 import dati from "../../api/specificheProdotti.json"
 
 
 
-function ScroolImage(){
-  
-  
-    const [selectedIndex, setSelectedIndex] = React.useState(0);
-  
-    function ChangeImage(index){
-      setSelectedIndex(index);
-    }
 
-    function shouldLoadComponent (index){
-        return (index === selectedIndex);
-    }
+function ScroolImage(props) {
+  console.log(props.numeroRender)
+  const numeroRender = props.numeroRender
 
-    function BottomRadioGroup (props) {
-      return (
-        <RadioGroup
-          {...props}
-          style = {[props.styles , styles.RadioGroup]}
-          selectedIndex={selectedIndex}
-          onChange={ChangeImage}>
-        <Radio>1</Radio>
-        <Radio>2</Radio>
-      </RadioGroup>
-      );
-    }
-  
+
+  const [selectedIndex, setSelectedIndex] = React.useState(0);
+
+  function ChangeImage(index) {
+    setSelectedIndex(index);
+  }
+
+  function shouldLoadComponent(index) {
+    return (index === selectedIndex);
+  }
+
+  function BottomRadioGroup(props) {
+    
     return (
-      <>
-        <Card style = {{margin : 26 , borderRadius : 10}} status = 'primary' footer = {BottomRadioGroup} disabled = {true}>
-        <ViewPager
+      <RadioGroup
+        {...props}
+        style={[props.styles, styles.RadioGroup]}
         selectedIndex={selectedIndex}
-        shouldLoadComponent = {shouldLoadComponent}
-        onSelect={ChangeImage}>
-        <Layout
-          style={styles.imageContiner}
-          level='1'>
-          <Image 
-            style = {styles.image}
-            source = {dati.data[props.numeroRender].linkImmagini}/>
-        </Layout>
-        <Layout
-          style={styles.imageContiner}
-          level='1'>
-           <Image 
-            style = {styles.image}
-            source = {dati.data[props.numeroRender].linkImmagini2}/>
-        </Layout>
-      </ViewPager>
-      </Card>
-      </>
-
+        onChange={ChangeImage}>
+          {dati.data[numeroRender].linkImmagini.map(el => <Radio/>)}
+      </RadioGroup>
     );
   }
 
-  export default ScroolImage;
+  return (
+    <>
+      <Card style={{ margin: 26, borderRadius: 10 }} status='primary' footer={BottomRadioGroup} disabled={true}>
+        <ViewPager
+          selectedIndex={selectedIndex}
+          shouldLoadComponent={shouldLoadComponent}
+          onSelect={ChangeImage}>
+          <Layout
+            style={styles.imageContiner}
+            level='1'>
+              {dati.data[numeroRender].linkImmagini.map(el => <Image style={styles.image} source={dati.data[numeroRender].linkImmagini[el]}/>)}
+          </Layout>
+        </ViewPager>
+      </Card>
+    </>
+
+  );
+}
+
+export default ScroolImage;
 
